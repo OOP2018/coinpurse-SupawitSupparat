42d8469009e76e205f7b6ab81198a498df2caee8
Supawit Supparat <Supawit.sup@ku.th>
2018-01-19 14:16:28 +0700

    add coin class

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -1,5 +1,59 @@
 package coinpurse;
+/**
+* Coin represents coinage (money) with a fixed value and currency.
+* @author Bill Gates
+*/
 
-public class Coin {
+public class Coin implements Comparable<Coin>{
 
+	double value;
+	String currency;
+	
+	Coin(double value, String currency)
+	{
+		value = 0.5;
+		currency = "Dollar";
+	}
+	
+	public double getValue( )
+	{
+		return this.value;
+		
+	}
+	
+	public String getCurrency() 
+	{
+		return this.currency;
+	}
+	
+	public boolean equals(Object arg) 
+	{
+		if (arg == null) return false;
+		if (arg.getClass() != this.getClass() )
+			return false;
+		Coin other = (Coin) arg;
+		if ( currency.equalsIgnoreCase( other.currency ) && value==other.value )
+			return true;
+		return false; 
+		
+	}
+	
+	
+	public int compareTo(Coin coin) 
+	{
+		if(coin.compareTo(coin)<0)
+		{
+			return  -1;
+		}
+		else if(coin.compareTo(coin)>0)	
+		{
+			return 1;
+		}
+		else return 0;
+	}
+	
+	public String toString()
+	{
+		return value + " " + currency;
+	}
 }
